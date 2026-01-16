d9a54c3d615ae9125472b28cc40be94058ed74ff
Luis Henrique <lhfhenrique@hotmail.com>
2026-01-16 16:55:02 -0300

    ajustes

---
+++ Movies.API/Movies.API/GraphQL/Types/MovieReviewType.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+using Movies.API.Models;
+
+namespace Movies.API.GraphQL.Types
+{
+    public class MovieReviewType:ObjectGraphType<MovieReview>
+    {
+        public MovieReviewType()
+        {
+            Field(a => a.Id).Description("Id of review");
+            Field(a => a.Rate).Description("Rate of review");
+            Field(a => a.Comment).Description("Commet of review");
+            Field(a => a.CreateAt).Description("Date of Create of review");
+            Field(a => a.MovieId).Description("Id of movie");
+          }
+    }
+}
---
+++ Movies.API/Movies.API/GraphQL/Types/MovieType.cs
@@ -0,0 +1,27 @@
+﻿using GraphQL.Types;
+using Movies.API.GraphQL.Types.Enums;
+using Movies.API.Models;
+using Ext = GraphQL.Types;
+
+namespace Movies.API.GraphQL.Types
+{
+    public class MovieType: ObjectGraphType<Movie>
+    {
+        public MovieType()
+        {
+           //Ext.IntGraphType
+            
+            Field(a => a.Id).Description("Id of movie");
+            Field(a => a.Name).Description("Name of movie");
+            Field(a => a.Description).Description("derscription of movie");
+            Field(a => a.LunchDate).Description("lauch of movie");
+
+            // mapeou o tipo enum
+            Field<MovieGenreType>("Genre").Description("genre of movie");
+            
+            //mapeou o tipo list objt
+            Field<MovieReviewType>("Reviews").Description("review of movie");
+            
+        }
+    }
+}
